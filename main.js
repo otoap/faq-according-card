aa502edbed9c4f051a8c0d47bfe165d3d6ed2ef2
otoap <Otaraptsiauri@gmail.com>
2024-05-10 16:36:08 +0400

    add js and start work to desktop screen

---
+++ main.js
@@ -0,0 +1,38 @@
+"use strict"
+
+let arrow = document.getElementsByClassName("arrow");
+let p = document.getElementsByTagName("p");
+let li = document.getElementsByTagName("li");
+
+let pArr = Array.from(p)
+let arrowArr = Array.from(arrow);
+let arrowLi = Array.from(li)
+
+console.log(arrowArr)
+console.log(window.getComputedStyle(pArr[2]).display)
+console.log(arrowLi)
+
+
+arrowArr.forEach((arrow, index) => {
+    arrow.addEventListener("click", ()=> {
+        if(window.getComputedStyle(p[index]).display !== "none") {
+            p[index].style.display = "none"
+            arrowLi[index].style.fontWeight = "normal"
+        }
+
+
+        pArr.forEach(element => {
+            element.style.display = "none"
+        })
+        arrowLi.forEach(element => {
+            element.style.fontWeight = "normal"
+        })
+        arrowArr.forEach(element => {
+            element.style.transform = "rotate(0deg)"
+        })
+
+        li[index].style.fontWeight = "bold";
+        p[index].style.display = "block";
+        arrowArr[index].style.transform = "rotate(180deg)"
+    })
+})
